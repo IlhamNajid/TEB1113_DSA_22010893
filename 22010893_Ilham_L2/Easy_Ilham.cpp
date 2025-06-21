9ce08eb3467979e6d076fbc3e985e239955102ed
IlhamNajid <milhamnajid@gmail.com>
2025-06-21 18:50:55 +0800

    Add files via upload

---
+++ 22010893_Ilham_L2/Easy_Ilham.cpp
@@ -0,0 +1,41 @@
+#include <iostream>
+using namespace std;
+
+void computeDiagonalSums(int matrix[][100], int n)
+{
+    int principalDiagonalSum = 0;
+    int secondaryDiagonalSum = 0;
+
+    for (int i = 0; i < n; i++)
+    {
+        principalDiagonalSum += matrix[i][i];
+        secondaryDiagonalSum += matrix[i][n - i - 1];
+    }
+
+    cout << "Output:\n";
+    cout << "Primary Diagonal Sum: " << principalDiagonalSum << endl;
+    cout << "Secondary Diagonal Sum: " << secondaryDiagonalSum << endl;
+}
+
+int main()
+{
+    int n;
+
+    cout << "Input: ";
+    cin >> n;
+
+    int matrix[n][n];
+
+    cout << "Enter the elements of the matrix:\n";
+    for (int i = 0; i < n; i++)
+    {
+        for (int j = 0; j < n; j++)
+        {
+            cin >> matrix[i][j];
+        }
+    }
+
+    computeDiagonalSums(matrix, n);
+
+    return 0;
+}
